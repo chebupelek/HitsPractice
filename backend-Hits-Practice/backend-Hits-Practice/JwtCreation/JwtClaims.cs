dc5b290d6151d19cd52b091a5d3ecc017733b610
chebupelek <mistiquelico@gmail.com>
2024-09-23 21:20:47 +0700

    Добавлены методы создания токенов

---
+++ backend-Hits-Practice/backend-Hits-Practice/JwtCreation/JwtClaims.cs
@@ -0,0 +1,17 @@
+﻿using Events.EventsDbModels;
+using System.Security.Claims;
+
+namespace Events.JwtCreation
+{
+    public static class JwtClaims
+    {
+        public static List<Claim> CreateClaims(this UserDbModel user)
+        {
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            };
+            return claims;
+        }
+    }
+}
