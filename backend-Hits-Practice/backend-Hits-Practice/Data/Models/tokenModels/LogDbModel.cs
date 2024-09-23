d715c3ba616e581f8076a105dbdca3025a2981d9
chebupelek <mistiquelico@gmail.com>
2024-09-23 21:08:12 +0700

    Сущность токена

---
+++ backend-Hits-Practice/backend-Hits-Practice/Data/Models/tokenModels/LogDbModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Events.TokenDbModels;
+
+public class LogDbModel
+{
+    [Key]
+    public required Guid Id { get; set; }
+    public required Guid UserId { get; set; }
+    public required DateTime LogDate { get; set; }
+    public required string Token { get; set; }
+    public required bool IsLog { get; set; }
+}
